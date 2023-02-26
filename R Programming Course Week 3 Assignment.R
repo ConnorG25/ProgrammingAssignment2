a075a9ac2885d3da9d5a13ed28061f96320547b2
ConnorG25 <95015922+ConnorG25@users.noreply.github.com>
2023-02-26 08:35:13 -0800

    Add files via upload

---
+++ R Programming Course Week 3 Assignment.R
@@ -0,0 +1,46 @@
+# Your assignment is to write a pair of functions that cache the inverse of a matrix.
+
+# Write the following functions:
+
+# 1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+# 2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+      #If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+      #retrieve the inverse from the cache.
+
+# Computing the inverse of a square matrix can be done with the solve function in R. 
+#    For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
+# For this assignment, assume that the matrix supplied is always invertible.
+
+library(MASS)
+makeCacheMatrix <- function(x = matrix()) {
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function()x
+      setinv <- function(inverse)inv<<-inverse
+      getinv <- function() {
+            inver <- ginv(x)
+            inver%*%x
+            }
+      list(set = set, get = get,
+            setinv = setinv,
+            getinv = getinv)
+}
+
+cacheSolve <- function(x, ...) {
+      inv <- x$getinv()
+      if(!is.null(inv)) {
+            return(inv)
+      }
+      data<-x$get()
+      inv<-solve(data,...)
+      x$setinv(inv)
+      inv
+}
+      
+
+
